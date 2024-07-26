b7d70ebfe911bd0d7e320fc62f3be1bff74a4844
CodeMnh <minhkhac1002@gmail.com>
2024-07-26 18:07:29 +0700

    Lấy data sản phẩm in ra giao diện

--- controllers/admin/products.controller.js
+++ controllers/admin/products.controller.js
@@ -1,6 +1,16 @@
 // [GET] /admim/products
-module.exports.index = (req, res) => {
+
+const Products = require("../../model/products.model");
+
+module.exports.index = async (req, res) => {
+    const products = await Products.find({
+        deleted: false
+    })
+
+    console.log(products);
+
     res.render('admin/pages/products/index', {
-        pageTitle : "Trang chủ"
+        pageTitle : "Trang chủ",
+        products : products
     });
 }
