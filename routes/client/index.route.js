88b56f86c17cf209a53c4b799e7ce261078ff737
CodeMnh <minhkhac1002@gmail.com>
2024-12-07 12:42:14 +0700

    menu san pham trang client

--- routes/client/index.route.js
+++ routes/client/index.route.js
@@ -1,7 +1,9 @@
 const productRoutes = require("./product.route");
 const homeRoutes = require("./home.route");
+const productsCategoryMiddlewate = require("../../middlewares/clients/productsCategory.middleware")
 
 module.exports = (app) => {
+    app.use(productsCategoryMiddlewate.productsCategory)
     app.use('/', homeRoutes);
     app.use('/products', productRoutes);
 }
