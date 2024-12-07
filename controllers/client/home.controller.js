6a62d413221338a6fd94dd891b047d85136b2939
CodeMnh <minhkhac1002@gmail.com>
2024-12-07 14:19:23 +0700

    hiển thị sản phẩm mới trang client

--- controllers/client/home.controller.js
+++ controllers/client/home.controller.js
@@ -2,6 +2,7 @@ const Product = require('../../model/products.model');
 const productsHellper = require("../../helpers/products")
 // [GET] /
 module.exports.index = async (req, res) => {
+    // lấy ra sản phẩm nổi bật
     let find = {
         deleted : false,
         featured : "1",
@@ -9,10 +10,18 @@ module.exports.index = async (req, res) => {
     }
     const productsFeatured = await Product.find(find).limit(6);
     const newProductsFeatured = productsHellper.priceNewProduct(productsFeatured);
-    // console.log(productsFeatured);
+    // end lấy ra sản phẩm nổi bật
+
+    const productsNew = await Product.find({
+        deleted : false,
+        status : "active"
+    }).sort({position : "desc"}).limit(6)
+    const newProductsNew = productsHellper.priceNewProduct(productsNew);
+
 
     res.render('client/pages/home/index', {
         pageTitle : "Trang chủ",
-        productsFeatured : newProductsFeatured
+        productsFeatured : newProductsFeatured,
+        productsNew : newProductsNew
     });
 }
