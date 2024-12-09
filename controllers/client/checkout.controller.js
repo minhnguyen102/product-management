ef4c6c1515d176884c5b8bdbbfbeb440b2cff3bb
CodeMnh <minhkhac1002@gmail.com>
2024-12-09 16:54:16 +0700

    Lưu thông tin đặt hàng vào database

--- controllers/client/checkout.controller.js
+++ controllers/client/checkout.controller.js
@@ -1,5 +1,6 @@
 const Cart = require("../../model/cart.model")
 const Product = require("../../model/products.model")
+const Order = require("../../model/order.model")
 const ProductHelper = require("../../helpers/products")
 
 module.exports.index = async (req, res) =>{
@@ -31,4 +32,56 @@ module.exports.index = async (req, res) =>{
         pageTitle : "Trang đặt hàng",
         cartDetail : cart
     });
+}
+
+module.exports.order = async (req, res) =>{ 
+    const cartId = req.cookies.cartId;
+    const userInfo = req.body;
+
+    const cart = await Cart.findOne({
+        _id : cartId
+    })
+
+    let products = []
+    for(const product of cart.products ){
+        const objectProduct = {
+            product_id : product.product_id,
+            price : 0,
+            discountPercentage : 0,
+            quantity : product.quantity
+        }
+        const productInfo = await Product.findOne({
+            _id : product.product_id
+        })
+
+        objectProduct.price = productInfo.price;
+        objectProduct.discountPercentage = productInfo.discountPercentage;
+        products.push(objectProduct);
+
+// Cap nhat lai sp con lai
+
+        const newStock = productInfo.stock - product.quantity;
+        await Product.updateOne({
+            _id : product.product_id
+        },{
+            stock : newStock
+        })
+    }
+// End
+
+    const objectOrder = {
+        cart_id :cartId,
+        userInfo :userInfo,
+        products : products
+    }
+
+    const order = new Order(objectOrder);
+    await order.save();
+
+    await Cart.updateOne({
+        _id : cartId
+    },{
+        products : []
+    })
+    res.redirect(`/checkout/success/${order.id}`)
 }
