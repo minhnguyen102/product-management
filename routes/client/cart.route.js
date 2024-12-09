384e09c66ef907949f1299d9e154b1a959943d6b
CodeMnh <minhkhac1002@gmail.com>
2024-12-09 15:27:15 +0700

    Cập nhật số lượng sảnt phẩm thành công

--- routes/client/cart.route.js
+++ routes/client/cart.route.js
@@ -3,9 +3,10 @@ const router = express.Router();
 
 const controller = require('../../controllers/client/cart.controller');
 
-// router.get("/", controller.index)
-// router.get("/delete/:productId", controller.delete)
+router.get("/", controller.index)
+router.get("/delete/:productId", controller.delete)
 
-// router.post('/add/:productId', controller.addPost);
+router.post('/add/:productId', controller.addPost);
+router.get('/update/:productId/:quantity', controller.update);
 
 module.exports = router; // cho phép các file khác được require
