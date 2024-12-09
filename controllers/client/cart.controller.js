a60224a1be4049364b617a0558cd2dc76f957a66
CodeMnh <minhkhac1002@gmail.com>
2024-12-09 10:39:23 +0700

    Finish them san pham vao gio hang

--- controllers/client/cart.controller.js
+++ controllers/client/cart.controller.js
@@ -5,22 +5,44 @@ module.exports.addPost = async (req, res) => {
     
     const cartId = req.cookies.cartId;
     const productId = req.params.productId;
-    const quantity = req.body.quantity;
+    const quantity = parseInt(req.body.quantity);
+    // console.log(quantity);
 
-    const objectCart = {
-        product_id : productId,
-        quantity : quantity
+    const cart = await Cart.findOne({
+        _id : cartId
+    });
+    // console.log(cart.products);
+
+    const exitProductId = cart.products.find(item => item.product_id == productId)
+    // console.log(exitProductId);
+
+    if(exitProductId){
+        //nếu đã có sản phẩm đó trong giỏ hàng => Cập nhật số lương
+        // console.log("Cap nhat so luong")
+        const newQuantity = quantity + exitProductId.quantity;
+        // console.log(newQuantity);
+        await Cart.updateOne({
+            _id : cartId,
+            'products.product_id' : productId
+        },{
+            'products.$.quantity' : newQuantity
+        })
+    }else{
+        const objectCart = {
+            product_id : productId,
+            quantity : quantity
+        }
+
+        await Cart.updateOne(
+            {
+                _id : cartId,
+            },
+            {
+                $push : {products : objectCart}
+            }
+        )
     }
 
-    await Cart.updateOne(
-        {
-            _id : cartId,
-        },
-        {
-            $push : {products : objectCart}
-        }
-    )
-    // res.send("OK")
     req.flash('success', `Thêm mới sản phẩm thành công`);
     res.redirect(`back`)
 }
