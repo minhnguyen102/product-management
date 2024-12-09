c56946faef02c2c138e072a8bb023f5bb9aefab1
CodeMnh <minhkhac1002@gmail.com>
2024-12-09 13:54:59 +0700

    Làm trang giỏ hàng

--- controllers/client/cart.controller.js
+++ controllers/client/cart.controller.js
@@ -1,4 +1,6 @@
 const Cart = require("../../model/cart.model")
+const Product = require("../../model/products.model")
+const ProductHelper = require("../../helpers/products")
 
 // [POST] /cart/add/:productId
 module.exports.addPost = async (req, res) => {
@@ -45,4 +47,34 @@ module.exports.addPost = async (req, res) => {
 
     req.flash('success', `Thêm mới sản phẩm thành công`);
     res.redirect(`back`)
+}
+
+// [GET] /cart/
+module.exports.index = async (req, res) =>{
+    const cartId = req.cookies.cartId;
+
+    const cart = await Cart.findOne({
+        _id : cartId
+    })
+    // console.log(cart)
+
+    if(cart.products.length > 0){
+        for(const item of cart.products){
+            const productId = item.product_id;
+
+            let productInfo = await Product.findOne({
+                _id : productId
+            })
+            productInfo.priceNew = ProductHelper.priceNew(productInfo)
+            item.productInfo = productInfo;
+            item.totalPrice = item.quantity * productInfo.priceNew;
+        }
+    }
+
+    cart.totalPrice = cart.products.reduce((sum, item) => sum + item.totalPrice, 0)
+
+    res.render('client/pages/cart/index', {
+        pageTitle : "Trang giỏ hàng",
+        cartDetail : cart
+    });
 }
