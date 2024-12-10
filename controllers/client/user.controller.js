bd4c0937a38b9d06b563ac8a67d99216fc3dd54a
CodeMnh <minhkhac1002@gmail.com>
2024-12-10 22:29:31 +0700

    trang đăng nhập bên client

--- controllers/client/user.controller.js
+++ controllers/client/user.controller.js
@@ -1,5 +1,7 @@
 const md5 = require("md5")
 const User = require("../../model/user.model")
+
+
 // [GET] /user/register
 module.exports.register = async (req, res) => {
     res.render('client/pages/user/register', {
@@ -10,7 +12,8 @@ module.exports.register = async (req, res) => {
 module.exports.registerPost = async (req, res) => {
     console.log(req.body);
     const exisEmail = await User.findOne({
-        email : req.body.email
+        email : req.body.email,
+        deleted : false
     })
     if(exisEmail){
         req.flash('error', `Email đã tồn tại. Vui lòng đăng ký bằng email khác`);
@@ -27,4 +30,41 @@ module.exports.registerPost = async (req, res) => {
 
     res.redirect("/")
 
+}
+// [GET] /user/login
+module.exports.login = async (req, res) => {
+    res.render('client/pages/user/login', {
+        pageTitle : "Trang đăng nhập",
+    });
+}
+// [POST] /user/loginPost
+module.exports.loginPost = async (req, res) => {
+    // console.log(req.body);
+    const email = req.body.email;
+    const password = req.body.password;
+
+    const user = await User.findOne({
+        email : email,
+        deleted : false
+    })
+
+    if(!user){
+        req.flash('error', `Email không tồn tại.`);
+        res.redirect(`back`);
+        return;
+    }
+    if(md5(password) != user.password){
+        req.flash('error', `Mật khẩu không đúng.`);
+        res.redirect(`back`);
+        return;
+    }
+    if(user.status == "inactive"){
+        req.flash('error', `Tài khoản đã bị tạm khóa.`);
+        res.redirect(`back`);
+        return;
+    }
+
+    res.cookie("tokenUser", user.tokenUser)
+
+    res.redirect("/")
 }
