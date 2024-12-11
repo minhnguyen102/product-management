76e969ce2777431f7914660c6e605ee4e0b87818
CodeMnh <minhkhac1002@gmail.com>
2024-12-11 15:15:36 +0700

    Thay doi logo

--- middlewares/clients/user.middleware.js
+++ middlewares/clients/user.middleware.js
@@ -1,7 +1,7 @@
 const User = require("../../model/user.model");
 
 module.exports.infoUser = async (req, res, next) => {
-    console.log(req.cookies.tokenUser);
+    // console.log(req.cookies.tokenUser);
     if(req.cookies.tokenUser){
         const user = await User.findOne({
             tokenUser : req.cookies.tokenUser,
